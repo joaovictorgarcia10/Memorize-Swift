bab7a244639beb30dbb8c3436bcb709130ed3313
Joao Santana <joao.santana@rarolabs.com.br>
2022-10-26 17:23:01 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by João Victor Garcia on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
